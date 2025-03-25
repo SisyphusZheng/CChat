88c0e352d26aa9037354fd30cff5f10a84505d3c
李嘉图·M·路 <146103794+Ricardo-M-Zheng@users.noreply.github.com>
2025-03-26 02:43:54 +0800

    axios

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q9aucthvin8"
+    "revision": "0.sp236fl2cgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
